a857d5cc81c6655708b95e817068b6b9283e6567
chahuigwang <chahuigwang@gmail.com>
2025-01-23 13:19:03 +0900

    Update boj11399.py

--- Greedy/boj11399.py
+++ Greedy/boj11399.py
@@ -2,10 +2,8 @@ n = int(input())
 
 p = list(map(int, input().split()))
 p.sort()
-
-time = [0]
-
-for i in range(n):
-    time.append(time[-1] + p[i]) # 본인 전까지 + 본인 시간
     
-print(sum(time))
+for i in range(1, n):
+    p[i] = p[i-1] + p[i] # 본인 전까지 + 본인 시간
+
+print(sum(p))
