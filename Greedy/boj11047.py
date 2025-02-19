33b4011825869fa4f3f61fad43b90110546cee47
chahuigwang <chahuigwang@gmail.com>
2025-02-19 23:57:55 +0900

    Create boj11047.py

---
+++ Greedy/boj11047.py
@@ -0,0 +1,12 @@
+n, k = map(int, input().split())
+
+coins = [int(input()) for _ in range(n)]
+coins.reverse()
+
+cnt = 0
+for coin in coins:
+    if coin <= k:
+        cnt += k // coin
+        k = k % coin
+
+print(cnt)
