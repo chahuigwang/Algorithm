21a08481be684377fd84533252c5038054c94302
chahuigwang <chahuigwang@gmail.com>
2025-03-05 21:07:23 +0900

    Create 상하좌우.py

--- Implementation/상하좌우.py
+++ Implementation/상하좌우.py
@@ -1,21 +1,21 @@
 n = int(input())
 plans = input().split()
+
 x, y = 1, 1
 
-move = ['L', 'R', 'U', 'D']
+moves = ['L', 'R', 'U', 'D']
 dx = [0, 0, -1, 1]
 dy = [-1, 1, 0, 0]
 
 for plan in plans:
-
-    for i in range(len(move)):
-        if plan == move[i]:
+    for i in range(len(moves)):
+        if plan == moves[i]:
             nx = x + dx[i]
             ny = y + dy[i]
-
+            
     if nx < 1 or ny < 1 or nx > n or ny > n:
         continue
-
+        
     x, y = nx, ny
-
+    
 print(x, y)
