ccffe35e717f844257640b0d8d72de84a139be93
chahuigwang <chahuigwang@gmail.com>
2025-03-05 21:33:21 +0900

    Create 왕실의 나이트.py

--- Implementation/왕실의 나이트.py
+++ Implementation/왕실의 나이트.py
@@ -1,16 +1,18 @@
 location = input()
-x = int(location[1])
-y = int(ord(location[0])-ord('a')) + 1
 
+x = int(location[1])
+y = ord(location[0]) - ord('a') + 1
 
 dx = [-1, -2, -2, -1, 1, 2, 2, 1]
 dy = [-2, -1, 1, 2, 2, 1, -1, -2]
 
-count = 8
+cnt = 0
 for i in range(8):
     nx = x + dx[i]
     ny = y + dy[i]
     if nx < 1 or ny < 1 or nx > 8 or ny > 8:
-        count -= 1
-
-print(count)
+        continue
+    else:
+        cnt += 1
+        
+print(cnt)
