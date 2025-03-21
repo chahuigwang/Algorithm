c5515d91d1185ede0088517fbe4199c3233450ef
chahuigwang <chahuigwang@gmail.com>
2025-03-21 17:34:26 +0900

    Create boj11724.py

---
+++ DFS & BFS/boj11724.py
@@ -0,0 +1,32 @@
+import sys
+sys.setrecursionlimit(10**6)
+
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+graph = [[] for _ in range(n+1)]
+visited = [False] * (n+1)
+for _ in range(m):
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+    
+def dfs(graph, v, visited):
+    if visited[v]:
+        return False
+    
+    visited[v] = True
+    for neighbor in graph[v]:
+        if not visited[neighbor]:
+            dfs(graph, neighbor, visited)
+            
+    return True
+    
+
+count = 0
+for i in range(1, n+1):
+    if dfs(graph, i, visited):
+        count += 1
+        
+print(count)
