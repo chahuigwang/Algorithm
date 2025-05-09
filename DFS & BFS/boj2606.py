4430a2bc1936ac0077f951efaafc6f10e4ec0c08
chahuigwang <chahuigwang@gmail.com>
2025-05-09 21:04:20 +0900

    Update boj2606.py

--- DFS & BFS/boj2606.py
+++ DFS & BFS/boj2606.py
@@ -1,20 +1,22 @@
 def dfs(graph, v, visited):
-    visited.append(v)
+    visited[v] = True
+    count = 1
     
     for neighbor in graph[v]:
-        if neighbor not in visited:
-            dfs(graph, neighbor, visited)
-            
+        if not visited[neighbor]:
+            count += dfs(graph, neighbor, visited)
+
+    return count
+
 n = int(input())
 m = int(input())
 
 graph = [[] for _ in range(n+1)]
-for _ in range(m):
-    a, b = map(int, input().split())
-    graph[a].append(b)
-    graph[b].append(a)
-    
-visited = []
+visited = [False] * (n+1)
 
-dfs(graph, 1, visited)
-print(len(visited)-1)
+for _ in range(m):
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+    
+print(dfs(graph, 1, visited)-1)
