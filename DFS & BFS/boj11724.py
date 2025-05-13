f591b9d3c7a947d32b18e80ae3e6afffcdcf14aa
chahuigwang <chahuigwang@gmail.com>
2025-05-13 19:09:23 +0900

    Update boj11724.py

--- DFS & BFS/boj11724.py
+++ DFS & BFS/boj11724.py
@@ -1,17 +1,6 @@
 import sys
 sys.setrecursionlimit(10**6)
 
-input = sys.stdin.readline
-
-n, m = map(int, input().split())
-
-graph = [[] for _ in range(n+1)]
-visited = [False] * (n+1)
-for _ in range(m):
-    u, v = map(int, input().split())
-    graph[u].append(v)
-    graph[v].append(u)
-    
 def dfs(graph, v, visited):
     if visited[v]:
         return False
@@ -22,8 +11,17 @@ def dfs(graph, v, visited):
             dfs(graph, neighbor, visited)
             
     return True
-    
 
+input = sys.stdin.readline
+n, m = map(int, input().split())
+
+graph = [[] for _ in range(n+1)]
+visited = [False] * (n+1)
+for _ in range(m):
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+    
 count = 0
 for i in range(1, n+1):
     if dfs(graph, i, visited):
