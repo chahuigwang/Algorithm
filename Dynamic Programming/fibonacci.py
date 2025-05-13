43aab1b1fb9bb8ef7ec8ad5eb75aa734f3f225db
chahuigwang <chahuigwang@gmail.com>
2025-05-13 19:36:23 +0900

    Update fibonacci.py

--- Dynamic Programming/fibonacci.py
+++ Dynamic Programming/fibonacci.py
@@ -1,6 +1,6 @@
-def fibonacci(n):
+def fibo(n):
     if n == 1 or n == 2:
         return 1
-    return fibonacci(n - 1) + fibonacci(n - 2)
+    return fibo(n - 1) + fibo(n - 2)
 
-print(fibonacci(4))
+print(fibo(4))
