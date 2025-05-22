545a6d2e8dcf4d9f6a456740fc32860032881cf3
chahuigwang <chahuigwang@gmail.com>
2025-05-23 02:52:03 +0900

    Update boj2294.py

--- Dynamic Programming/boj2294.py
+++ Dynamic Programming/boj2294.py
@@ -2,12 +2,12 @@ n, k = map(int, input().split())
 
 coins = [int(input()) for _ in range(n)]
 
-d = [100001]*(k+1)
+d = [10001]*(k+1)
 d[0] = 0
 
 for coin in coins:
     for i in range(coin, k+1):
-        if d[i-coin] != 100001:
+        if d[i-coin] != 10001:
             d[i] = min(d[i], d[i-coin] + 1)
             
-print(d[k] if d[k] != 100001 else -1)
+print(d[k] if d[k] != 10001 else -1)
