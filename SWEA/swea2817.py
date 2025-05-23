a521299c39c1f17cd6a23cfdd53af5e169dee79e
chahuigwang <chahuigwang@gmail.com>
2025-05-23 22:42:16 +0900

    Create swea2817.py

---
+++ SWEA/swea2817.py
@@ -0,0 +1,21 @@
+def dfs(idx, current_sum):
+    global count
+    if current_sum > k:
+        return
+    if idx == n:
+        if current_sum == k:
+            count += 1
+        return
+    
+    dfs(idx+1, current_sum + nums[idx])
+    dfs(idx+1, current_sum)
+
+T = int(input())
+for test_case in range(1, T+1):
+    n, k = map(int, input().split())
+    nums = list(map(int, input().split()))
+    
+    count = 0
+    dfs(0, 0)
+                
+    print(f'#{test_case} {count}')
