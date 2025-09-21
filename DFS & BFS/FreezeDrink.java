7158c43ceed730037e9416daafa06b312143d644
chahuigwang <chahuigwang@gmail.com>
2025-09-21 16:08:57 +0900

    Update FreezeDrink.java

--- DFS & BFS/FreezeDrink.java
+++ DFS & BFS/FreezeDrink.java
@@ -1,13 +1,17 @@
 import java.util.Scanner;
 
 public class FreezeDrink {
+    public static int n, m;
+    public static int frame[][];
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
+
+        n = sc.nextInt();
+        m = sc.nextInt();
         sc.nextLine();
 
-        int[][] frame = new int[n][m];
+        frame = new int[n][m];
         for(int i = 0; i < n; i++) {
             String row = sc.nextLine();
             for(int j = 0; j < m; j++) {
@@ -18,27 +22,23 @@ public class FreezeDrink {
         int count = 0;
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < m; j++) {
-                if(dfs(frame, i, j))
-                    count++;
+                if(dfs(i, j)) count++;
             }
         }
+
         System.out.println(count);
     }
-    public static boolean dfs(int[][] graph, int x, int y) {
-        int n = graph.length;
-        int m = graph[0].length;
 
-        if(x < 0 || x >= n || y < 0 || y >= m)
-            return false;
+    public static boolean dfs(int x, int y) {
+        if(x <= -1 || x >= n || y <= -1 || y >= m) return false;
 
-        if(graph[x][y] == 0) {
-            graph[x][y] = 1;
+        if(frame[x][y] == 0) {
+            frame[x][y] = 1;
 
-            dfs(graph, x-1, y);
-            dfs(graph, x+1, y);
-            dfs(graph, x, y-1);
-            dfs(graph, x, y+1);
-
+            dfs(x - 1, y);
+            dfs(x, y - 1);
+            dfs(x + 1, y);
+            dfs(x, y + 1);
             return true;
         }
         return false;
