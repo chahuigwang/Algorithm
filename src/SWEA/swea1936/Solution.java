d6c5dedca4188d26e79d48544f4b37e495e14543
chahuigwang <chahuigwang@gmail.com>
2026-01-12 22:20:31 +0900

    Solved SWEA1936

---
+++ src/SWEA/swea1936/Solution.java
@@ -0,0 +1,15 @@
+package SWEA.swea1936;
+
+import java.util.*;
+
+class Solution
+{
+    public static void main(String args[]) throws Exception
+    {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println( a > b ? 'A' : 'B');
+    }
+}
