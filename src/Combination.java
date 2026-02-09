d0ef5a43bd2ee905b38383dac30ba2e62b55b39f
chahuigwang <chahuigwang@gmail.com>
2026-02-10 00:50:20 +0900

    Update Combination

--- src/Combination.java
+++ src/Combination.java
@@ -1,25 +1,35 @@
 public class Combination {
 
-    static int[] arr = {1, 2, 3};
-    static int[] comb = new int[2];
+    static int[] elementList = {1, 2, 3};
+    static final int ELEMENT_COUNT = 3;
     static final int SELECT_COUNT = 2;
+    static int[] selectElementList = new int[SELECT_COUNT];
 
     public static void main(String[] args) {
         combination(0, 0);
     }
 
-    static void combination(int selectedCount, int start) {
-        if(selectedCount == SELECT_COUNT) {
-            for(int x : comb) {
-                System.out.print(x + " ");
+    static void combination(int selectIdx, int elementIdx) {
+        // 1. 모든 원소를 선택했다면 종료
+        if(selectIdx == SELECT_COUNT) {
+            for(int idx = 0; idx < SELECT_COUNT; idx++) {
+                System.out.print(selectElementList[idx] + " ");
             }
             System.out.println();
             return;
         }
 
-        for(int idx = start; idx < arr.length; idx++) {
-            comb[selectedCount] = arr[idx];
-            combination(selectedCount + 1, idx + 1);
+        // 2. 모든 원소를 확인했다면 종료
+        if(elementIdx == ELEMENT_COUNT) {
+            return;
         }
+
+        // 3. 원소 선택
+        selectElementList[selectIdx] = elementList[elementIdx];
+        combination(selectIdx + 1, elementIdx + 1);
+
+        // 4. 원소 선택 x
+        selectElementList[selectIdx] = 0; // 초기화
+        combination(selectIdx, elementIdx + 1);
     }
 }
