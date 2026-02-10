72d68ea873b43fa491d4bdb132d4f79c6f93f0fc
chahuigwang <chahuigwang@gmail.com>
2026-02-10 15:42:01 +0900

    Update PowerSet

--- src/PowerSet.java
+++ src/PowerSet.java
@@ -1,27 +1,31 @@
 public class PowerSet {
 
-    static int[] arr = {1, 2, 3};
-    static boolean[] selected = new boolean[arr.length];
+    static int[] elementList = {1, 2, 3};
+    static final int ELEMENT_COUNT = 3;
+    static boolean[] selected = new boolean[ELEMENT_COUNT];
 
     public static void main(String[] args) {
         powerSet(0);
     }
 
-    static void powerSet(int visitedIdx) {
-        if(visitedIdx == arr.length) {
-            for(int idx = 0; idx < arr.length; idx++) {
-                if (selected[idx]) {
-                    System.out.print(arr[idx] + " ");
+    public static void powerSet(int elementIdx) {
+        // 1. 모든 원소를 확인했다면 종료
+        if(elementIdx == ELEMENT_COUNT) {
+            for(int idx = 0; idx < ELEMENT_COUNT; idx++) {
+                if(selected[idx]) {
+                    System.out.print(elementList[idx] + " ");
                 }
             }
             System.out.println();
             return;
         }
 
-        selected[visitedIdx] = true;
-        powerSet(visitedIdx + 1);
+        // 2. 원소 선택
+        selected[elementIdx] = true;
+        powerSet(elementIdx + 1);
 
-        selected[visitedIdx] = false;
-        powerSet(visitedIdx + 1);
+        // 3. 원소 선택 x
+        selected[elementIdx] = false;
+        powerSet(elementIdx + 1);
     }
 }
