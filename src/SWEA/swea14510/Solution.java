b869d5ce23f252ee1e1ca849e9105edef26742dd
chahuigwang <chahuigwang@gmail.com>
2026-02-12 09:22:58 +0900

    Solved SWEA14510

--- src/SWEA/swea14510/Solution.java
+++ src/SWEA/swea14510/Solution.java
@@ -3,9 +3,6 @@ package SWEA.swea14510;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 import java.util.StringTokenizer;
 
 /**
