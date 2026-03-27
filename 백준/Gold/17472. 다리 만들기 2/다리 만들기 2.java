79cfa758dc38308dae82fce66b017ecde56a977a
chahuigwang <57646870+chahuigwang@users.noreply.github.com>
2026-03-27 15:21:22 +0900

    [Gold I] Title: 다리 만들기 2, Time: 68 ms, Memory: 11720 KB -BaekjoonHub

--- 백준/Gold/17472. 다리 만들기 2/다리 만들기 2.java
+++ 백준/Gold/17472. 다리 만들기 2/다리 만들기 2.java
@@ -1,12 +1,9 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 /**
