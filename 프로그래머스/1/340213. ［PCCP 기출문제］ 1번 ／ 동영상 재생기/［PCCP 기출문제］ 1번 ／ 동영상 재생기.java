264e1656c5296846c8e6760618dcc2bc8b702431
chahuigwang <57646870+chahuigwang@users.noreply.github.com>
2026-04-15 13:14:25 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 0.21 ms, Memory: 83.4 MB -BaekjoonHub

--- 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.java
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.java
@@ -1,87 +1,72 @@
 class Solution {
 	
-	int curMin;
-	int curSec;
+	int curSecTime;
 
-	int endMin;
-	int endSec;
+	int endSecTime;
 
-	int opStartMin;
-	int opStartSec;
+	int opStartSecTime;
 
-	int opEndMin;
-	int opEndSec;
+	int opEndSecTime;
 	
     public String solution(String video_len, String pos, String op_start, String op_end, String[] commands) {
-    	String[] curMinSec = pos.split(":");
-    	curMin = Integer.parseInt(curMinSec[0]);
-        curSec = Integer.parseInt(curMinSec[1]);
-        
-        String[] endMinSec = video_len.split(":");
-        endMin = Integer.parseInt(endMinSec[0]);
-        endSec = Integer.parseInt(endMinSec[1]);
-        
-        String[] opStartMinSec = op_start.split(":");
-        opStartMin = Integer.parseInt(opStartMinSec[0]);
-        opStartSec = Integer.parseInt(opStartMinSec[1]);
-        
-        String[] opEndMinSec = op_end.split(":");
-        opEndMin = Integer.parseInt(opEndMinSec[0]);
-        opEndSec = Integer.parseInt(opEndMinSec[1]);
-        
-        for(String cmd : commands) {
-        	skipOpening();
-        	if(cmd.equals("prev")) backward();
-        	else if(cmd.equals("next")) forward();
-        }
-        skipOpening();
-        
-        StringBuilder sb = new StringBuilder();
-        if(curMin < 10) sb.append(0).append(curMin);
-        else sb.append(curMin);
-        
-        sb.append(":");
-        
-        if(curSec < 10) sb.append(0).append(curSec);
-        else sb.append(curSec);
-        
-    	String answer = sb.toString();
+    	curSecTime = timeStringToSecTime(pos);
+    	
+    	endSecTime = timeStringToSecTime(video_len);
+    	
+    	opStartSecTime = timeStringToSecTime(op_start);
+    	
+    	opEndSecTime = timeStringToSecTime(op_end);
+    	
+    	for(String command : commands) {
+    		skipOpening();
+    		if(command.equals("prev")) backward();
+    		else if(command.equals("next")) forward();
+    	}
+    	skipOpening();
+    	
+    	String answer = secTimeToTimeString(curSecTime);
         return answer;
+    }
+    
+    private int timeStringToSecTime(String timeString) {
+    	String[] minSec = timeString.split(":");
+    	int min = Integer.parseInt(minSec[0]);
+    	int sec = Integer.parseInt(minSec[1]);
+    	
+    	return min*60 + sec;
+    }
+    
+    private String secTimeToTimeString(int secTime) {
+    	int min = secTime / 60;
+    	int sec = secTime % 60;
+    	
+    	StringBuilder sb = new StringBuilder();
+    	if(min < 10) sb.append(0).append(min);
+    	else sb.append(min);
+    	
+    	sb.append(":");
+    	
+    	
+    	if(sec < 10) sb.append(0).append(sec);
+    	else sb.append(sec);
+    	
+    	return sb.toString();
     }
     
     private void backward() {
     	// 현재 위치가 10초 미만인 경우 영상의 처음 위치로 이동
-    	if(curMin == 0 && curSec < 10) curSec = 0;
-    	else {
-    		if(curSec < 10) {
-    			curMin--;
-    			curSec += 50;
-    		}
-    		else curSec -= 10;
-    	}
+    	if(curSecTime < 10) curSecTime = 0;
+    	else curSecTime -= 10;
     }
     
     private void forward() {
     	// 남은 시간이 10초 미만일 경우 영상의 마지막 위치로 이동
-    	if((curMin == endMin && endSec - curSec < 10) || (curMin == endMin - 1 && curSec - endSec > 50)) {
-    		curMin = endMin;
-    		curSec = endSec;
-    	}
-    	else {
-    		if(curSec >= 50) {
-    			curMin++;
-    			curSec -= 50;
-    		}
-    		else curSec += 10;
-    	}
+    	if(endSecTime - curSecTime < 10) curSecTime = endSecTime;
+    	else curSecTime += 10;
     }
     
     private void skipOpening() {
-    	// (op_start ≤ 현재 재생 위치 ≤ op_end)인 경우 자동으로 오프닝이 끝나는 위치로 이동
-    	if(((opStartMin < curMin) || (opStartMin == curMin && opStartSec <= curSec)) &&
-    			((curMin < opEndMin) || (curMin == opEndMin && curSec <= opEndSec))) {
-    		curMin = opEndMin;
-    		curSec = opEndSec;
-    	}
+    	if(opStartSecTime <= curSecTime && curSecTime <= opEndSecTime)
+    		curSecTime = opEndSecTime;
     }
 }
