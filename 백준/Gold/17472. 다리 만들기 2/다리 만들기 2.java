1e185d156fe941b286ded62435a142d9cc8d8aab
chahuigwang <57646870+chahuigwang@users.noreply.github.com>
2026-04-20 17:44:54 +0900

    [Gold I] Title: 다리 만들기 2, Time: 68 ms, Memory: 11688 KB -BaekjoonHub

--- 백준/Gold/17472. 다리 만들기 2/다리 만들기 2.java
+++ 백준/Gold/17472. 다리 만들기 2/다리 만들기 2.java
@@ -7,7 +7,71 @@ import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	
+	static int height;
+	static int width;
+	static int[][] map;
+	
+	public static void main(String[] args) throws IOException {
+		init();
+		System.out.println(calcMinTotalBridgeLength());
+	}
+	
+	static void init() throws IOException {
+		st = new StringTokenizer(br.readLine().trim());
+		height = Integer.parseInt(st.nextToken());
+		width = Integer.parseInt(st.nextToken());
+		
+		map = new int[height][width];
+		for(int row = 0; row < height; row++) {
+			st = new StringTokenizer(br.readLine().trim());
+			for(int col = 0; col < width; col++) {
+				map[row][col] = Integer.parseInt(st.nextToken());
+			}
+		}
+	}
+	
+	static boolean[][] visited;
+	static int islandNo;
+	static int islandCount;
+	
+	static void makeAllIslands() {
+		visited = new boolean[height][width];
+		islandNo = 1;
+		for(int row = 0; row < height; row++) {
+			for(int col = 0; col < width; col++) {
+				if(!visited[row][col] && map[row][col] == 1) {
+					makeIsland(row, col, islandNo);
+					islandNo++;
+				}
+			}
+		}
+		islandCount = islandNo - 1;
+	}
+	
+	static final int[] DR = {-1, 1, 0, 0};
+	static final int[] DC = {0, 0, -1, 1};
+	static final int DIRECTION_COUNT = 4;
+	
+	static void makeIsland(int row, int col, int islandNo) {
+		visited[row][col] = true;
+		map[row][col] = islandNo;
+		
+		int nextRow, nextCol;
+		for(int direction = 0; direction < DIRECTION_COUNT; direction++) {
+			nextRow = row + DR[direction];
+			nextCol = col + DC[direction];
+			if(isInRange(nextRow, nextCol) && !visited[nextRow][nextCol] && map[nextRow][nextCol] == 1)
+				makeIsland(nextRow, nextCol, islandNo);
+		}
+	}
+	
+	static boolean isInRange(int row, int col) {
+		return 0 <= row && row < height && 0 <= col && col < width;
+	}
+	
 	static class Bridge implements Comparable<Bridge> {
 		int from;
 		int to;
@@ -25,82 +89,12 @@ public class Main {
 		}
 	}
 	
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static StringTokenizer st;
-	
-	static int height;
-	static int width;
-	
-	static int[][] map;
-	static boolean[][] visited;
-	
-	static final int[] DR = {-1, 1, 0, 0};
-	static final int[] DC = {0, 0, -1, 1};
-	static final int DIRECTION_COUNT = 4;
-	
-	static int islandCount;
-	
 	static List<Bridge> bridges = new ArrayList<>();
 	
-	static int[] parents;
-	
-	public static void main(String[] args) throws IOException {
-		init();
-		makeAllIslands();
-		makeAllBridges();
-		System.out.println(calcMinTotalBridgeLength());
-	}
-	
-	static void init() throws IOException {
-		st = new StringTokenizer(br.readLine().trim());
-		height = Integer.parseInt(st.nextToken());
-		width = Integer.parseInt(st.nextToken());
-		
-		map = new int[height][width];
-		for(int row = 0; row < height; row++) {
-			st = new StringTokenizer(br.readLine().trim());
-			for(int col = 0; col < width; col++) {
-				map[row][col] = Integer.parseInt(st.nextToken());
-			}
-		}
-		
-		visited = new boolean[height][width];
-	}
-	
-	static void makeAllIslands() {
-		int islandNo = 1;
-		for(int row = 0; row < height; row++) {
-			for(int col = 0; col < width; col++) {
-				if(!visited[row][col] && map[row][col] == 1) {
-					makeIsland(row, col, islandNo++);
-				}
-			}
-		}
-		islandCount = islandNo - 1;
-	}
-	
-	static void makeIsland(int row, int col, int islandNo) {
-		visited[row][col] = true;
-		map[row][col] = islandNo;
-		
-		int nextRow, nextCol;
-		for(int direction = 0; direction < DIRECTION_COUNT; direction++) {
-			nextRow = row + DR[direction];
-			nextCol = col + DC[direction];
-			if(isInRange(nextRow, nextCol) && !visited[nextRow][nextCol] && map[nextRow][nextCol] == 1) {
-				makeIsland(nextRow, nextCol, islandNo);
-			}
-		}
-	}
-	
-	static boolean isInRange(int row, int col) {
-		return 0 <= row && row < height && 0 <= col && col < width;
-	}
-	
 	static void makeAllBridges() {
-		int from, to;
 		int nextRow, nextCol;
 		int length;
+		int from, to;
 		for(int row = 0; row < height; row++) {
 			for(int col = 0; col < width; col++) {
 				if(map[row][col] != 0) {
@@ -108,21 +102,17 @@ public class Main {
 						nextRow = row + DR[direction];
 						nextCol = col + DC[direction];
 						if(isInRange(nextRow, nextCol) && map[nextRow][nextCol] == 0) {
-							nextRow += DR[direction];
-							nextCol += DC[direction];
-							length = 1;
+							length = 0;
 							while(isInRange(nextRow, nextCol)) {
 								if(map[nextRow][nextCol] != 0) {
-									if(length > 1) {
-										from = map[row][col];
-										to = map[nextRow][nextCol];
-										if(from != to) bridges.add(new Bridge(from, to, length));
-									}
+									from = map[row][col];
+									to = map[nextRow][nextCol];
+									if(from != to && length > 1) bridges.add(new Bridge(from, to, length));
 									break;
 								}
-								length++;
 								nextRow += DR[direction];
 								nextCol += DC[direction];
+								length++;
 							}
 						}
 					}
@@ -133,10 +123,12 @@ public class Main {
 		Collections.sort(bridges);
 	}
 	
+	static int[] parents;
+	
 	static void makeSets() {
 		parents = new int[islandCount + 1];
-		for(int islandNo = 1; islandNo <= islandCount; islandNo++) {
-			parents[islandNo] = islandNo;
+		for(int islandIdx = 1; islandIdx <= islandCount; islandIdx++) {
+			parents[islandIdx] = islandIdx;
 		}
 	}
 	
@@ -145,18 +137,21 @@ public class Main {
 		return parents[islandNo] = find(parents[islandNo]);
 	}
 	
-	static boolean union(int islandNo1, int islandNo2) {
-		int rootIsland1 = find(islandNo1);
-		int rootIsland2 = find(islandNo2);
+	static boolean union(int island1, int island2) {
+		int root1 = find(island1);
+		int root2 = find(island2);
 		
-		if(rootIsland1 == rootIsland2) return false;
+		if(root1 == root2) return false;
 		
-		parents[rootIsland2] = rootIsland1;
+		parents[root2] = root1;
 		return true;
 	}
 	
 	static int calcMinTotalBridgeLength() {
+		makeAllIslands();
+		makeAllBridges();
 		makeSets();
+		
 		int totalBridgeLength = 0;
 		int connectCount = 0;
 		for(Bridge bridge : bridges) {
