30f9f71760d44b07e62f585f948c520d90776223
chahuigwang <chahuigwang@gmail.com>
2026-07-22 00:21:54 +0900

    [level 1] Title: 최소직사각형, Time: 21.64 ms, Memory: 98.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+class Solution {
+
+    int[][] sortedSizes;
+    int cardCount;
+    
+    public int solution(int[][] sizes) {
+        cardCount = sizes.length;
+        sortSizes(sizes);
+        int answer = calculateMinWalletSize();
+        return answer;
+    }
+
+    void sortSizes(int[][] sizes) {
+        sortedSizes = new int[cardCount][2];
+        
+        int length1, length2;
+        for(int cardIdx = 0; cardIdx < cardCount; cardIdx++) {
+            length1 = sizes[cardIdx][0];
+            length2 = sizes[cardIdx][1];
+            
+            sortedSizes[cardIdx][0] = Math.max(length1, length2);
+            sortedSizes[cardIdx][1] = Math.min(length1, length2);
+        }
+    }
+    
+    int calculateMinWalletSize() {
+        int width = 0;
+        int height = 0;
+        
+        for(int cardIdx = 0; cardIdx < cardCount; cardIdx++) {
+            width = Math.max(width, sortedSizes[cardIdx][0]);
+        }
+
+        for(int cardIdx = 0; cardIdx < cardCount; cardIdx++) {
+            height = Math.max(height, sortedSizes[cardIdx][1]);
+        }
+        
+        return width * height;
+    }
+}
